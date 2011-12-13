7d8c93ac7f2d313beb812cc2793929faa3ffb0cb
Raphael Lisicki <raphael@mars.ralisi.de>
2011-12-13 01:10:15 -0200

    fiddle with testbench.

--- ram_tb.vhd
+++ ram_tb.vhd
@@ -21,8 +21,9 @@ port (
   );
 end component;
 
-signal data_in, data_out: std_logic_vector (7 downto 0);
-signal address: std_logic_vector (15 downto 0) :=  "0000000000000000";
+signal data_in : std_logic_vector (15 downto 0):=   (others => '0');
+signal data_out: std_logic_vector (7 downto 0);
+signal address: std_logic_vector (18 downto 0) :=  (others => '0');
 signal enable: std_logic := '0';
 signal clock: std_logic:='0';
 
@@ -31,12 +32,12 @@ begin
 myRam: sync_ram port map (clock, enable, address, data_in, data_out);
 clock   <= not clock  after 5 ns;
 
-data_in <= "UUUUUUUU", "00000000" after 19 ns, "10000100" after 40 ns;
+
 enable <= '1' after 5 ns, not enable  after 10 ns;
 
 
 address   <= address + 1  after 20 ns;
-
+data_in   <= data_in + 1  after 20 ns;
 
 
 end behav;
